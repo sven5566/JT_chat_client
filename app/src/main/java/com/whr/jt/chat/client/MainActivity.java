185523d5aa18ea75444a815972facbfa3b56301c
WuHaoran <whrr524@163.com>
2017-06-13 17:05:50 +0800

    主页面编码

--- app/src/main/java/com/whr/jt/chat/client/MainActivity.java
+++ app/src/main/java/com/whr/jt/chat/client/MainActivity.java
@@ -1,21 +1,39 @@
 package com.whr.jt.chat.client;
 
-import android.widget.Button;
-import android.widget.EditText;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
 import com.whr.common.app.Activity;
+import com.whr.common.app.widget.PortraitView;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 
-public class MainActivity extends Activity {
-	@BindView(R.id.edit_query)
-	EditText mEtQuery;
-	@BindView(R.id.btn_submit)
-	Button mBtnSubmit;
-	@BindView(R.id.tv_result)
-	TextView mTvResult;
+public class MainActivity extends Activity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+	@BindView(R.id.appbar)
+	View mLayAppbar;
+	@BindView(R.id.im_portrait)
+	PortraitView mPortrait;
+	@BindView(R.id.txt_title)
+	TextView mTitle;
+	@BindView(R.id.im_search)
+	ImageView imSearch;
+	@BindView(R.id.lay_container)
+	FrameLayout mContainer;
+
+	@BindView(R.id.navigation)
+	BottomNavigationView mNavigation;
+
 	@Override
 	protected int getContentId() {
 		return R.layout.activity_main;
@@ -24,10 +42,28 @@ public class MainActivity extends Activity {
 	@Override
 	protected void initWidgt() {
 		super.initWidgt();
+		mNavigation.setOnNavigationItemSelectedListener(this);
+		Glide.with(this).load(R.drawable.bg_src_morning).centerCrop().into(new ViewTarget<View, GlideDrawable>(mLayAppbar) {
+			@Override
+			public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+				this.view.setBackground(resource.getCurrent());
+			}
+		});
 	}
 
-	@OnClick(R.id.btn_submit)
+	@OnClick(R.id.btn_action)
 	public void clickBtn(){
+
 	}
 
+	@OnClick(R.id.im_search)
+	public void clickIm(){
+
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+		mTitle.setText(item.getTitle());
+		return false;
+	}
 }
---
+++ common/src/main/java/com/whr/common/app/widget/PortraitView.java
@@ -0,0 +1,24 @@
+package com.whr.common.app.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by whrr5 on 2017/6/13.
+ */
+
+public class PortraitView extends CircleImageView {
+	public PortraitView(Context context) {
+		super(context);
+	}
+
+	public PortraitView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public PortraitView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+}
